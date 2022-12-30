b1c0ab0e11f19c5b275afba41c356987a5b33989
panditvishal04 <panditvishal9546@gmail.com>
2022-12-30 12:42:07 +0530

    add arrow

--- sum.java
+++ sum.java
@@ -5,7 +5,7 @@ public static void main(String args[])
 {  
 int x, y, sum;  
 Scanner sc = new Scanner(System.in);  
-System.out.print("Enter the first number: ");  
+System.out.print("Enter the first number ->->: ");  
 x = sc.nextInt();  
 System.out.print("Enter the second number: ");  
 y = sc.nextInt();  
@@ -15,7 +15,7 @@ System.out.println("The sum of two numbers x and y is: " + sum);
 //method that calculates the sum  
 public static int sum(int c, int d)  
 {  
-int sum = c / d;  
+int sum = c + d;  
 return sum;  
 }  
 }  
